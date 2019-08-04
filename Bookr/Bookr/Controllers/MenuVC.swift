a69ceac7d8efc2ca042d5ebf7a64b77b35a9127f
Jeffrey Santana <SantanaJny@gmail.com>
2019-08-04 05:59:12 -0400

    Adding side menu

---
+++ Bookr/Bookr/Controllers/MenuVC.swift
@@ -0,0 +1,48 @@
+//
+//  MenuVC.swift
+//  Bookr
+//
+//  Created by Jeffrey Santana on 8/4/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import UIKit
+
+class MenuVC: UIViewController {
+	
+	//MARK: - IBOutlets
+	
+	
+	//MARK: - Properties
+	
+	
+	//MARK: - Life Cycle
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		
+	}
+	
+	//MARK: - IBActions
+	@IBAction func closeOnTap(_ sender: UITapGestureRecognizer) {
+		dismiss(animated: false, completion: nil)
+	}
+	
+	@IBAction func settingsBtnTapped(_ sender: Any) {
+		
+	}
+	
+	@IBAction func myBooksBtnTapped(_ sender: Any) {
+		
+	}
+	
+	@IBAction func exploreBtnTapped(_ sender: Any) {
+		
+	}
+	
+	//MARK: - Helpers
+	
+	
+
+}
